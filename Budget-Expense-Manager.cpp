ee7205a1fcf6a5969115662c8a2ef3b6b5debe7a
AkodiyaHimanshu <himanshu.a@turing.com>
2025-06-11 19:12:32 +0530

    first commit

---
+++ Budget-Expense-Manager.cpp
@@ -0,0 +1,12 @@
+﻿// Budget-Expense-Manager.cpp : Defines the entry point for the application.
+//
+
+#include "Budget-Expense-Manager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World." << endl;
+	return 0;
+}
